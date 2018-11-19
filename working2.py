59d6af3455255bce78ca50e295be980b7d66ee87
Deanna Gran <32341520+deannagran@users.noreply.github.com>
2018-11-19 15:18:13 -0500

    functional socket and voice control

--- working2.py
+++ working2.py
@@ -1,8 +1,39 @@
 import pyaudio,os
 import speech_recognition as sr
 from subprocess import call
+import socket   #for sockets
+import sys  #for exit
 
 call(['espeak', 'HELLO TREY. PLEASE ALLOW TWENTY TO THIRTY SECONDS FOR ME TO WARM UP....'])
+try:
+    #create an AF_INET, STREAM socket (TCP)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+except socket.error, msg:
+    print 'Failed to create socket. Error code: ' + str(msg[0]) + ' , Error message : ' + msg[1]
+    sys.exit();
+
+print 'Socket Created'
+
+host = '10.247.22.118' #Crestron Processor IP
+port = 5005 #The port specified in Simpl programs TCP/IP Server symbol
+
+try:
+    remote_ip = socket.gethostbyname( host )
+
+except socket.gaierror:
+    #could not resolve
+    print 'Hostname could not be resolved. Exiting'
+    sys.exit()
+
+print 'Ip address of ' + host + ' is ' + remote_ip
+
+#Connect to remote server
+s.connect((remote_ip , port))
+
+call(['espeak', 'SOCKET CONNECTED.'])
+print 'Socket Connected to ' + host + ' on ip ' + remote_ip
+
+
 
 def excel():
         #os.system("start excel.exe")
@@ -22,15 +53,20 @@ def mainfunction(source):
     try:
         user = r.recognize_google(audio, language = "en-us", show_all=False)
         print(user)
-        if user == "Excel":
-            call(['espeak', 'OPENING EXCEL..'])
+        if user == "turn on projector":
+            call(['espeak', 'TURNING ON PROJECTOR..'])
             excel()
-        elif user == "check":
-            call(['espeak', 'PERFORMING CHECK..'])
+            s.send('turn on projector')
+        elif user == "turn off lights":
+            call(['espeak', 'TURNING OFF LIGHTS...'])
             internet()
+            s.send('turning off lights')
         elif user == "media":
             call(['espeak', 'OPENING MEDIA NOW...'])
             media()
+            s.send('Open media')
+        else:
+            call(['espeak', 'NOT A VALID VOICE COMMAND. TRY AGAIN.'])
     except:
         print("Cannot understand you. Please repeat your command.")
         call(['espeak', 'CANNOT UNDERSTAND YOU. PLEASE REPEAT YOUR COMMAND...'])
