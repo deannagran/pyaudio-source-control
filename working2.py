b4a4c8550bb130d2b588decea34a29dbfe9c9422
Deanna Gran <32341520+deannagran@users.noreply.github.com>
2018-11-19 14:10:03 -0500

    added espeak capability

--- working2.py
+++ working2.py
@@ -1,6 +1,8 @@
 import pyaudio,os
 import speech_recognition as sr
+from subprocess import call
 
+call(['espeak', 'HELLO TREY. PLEASE ALLOW TWENTY TO THIRTY SECONDS FOR ME TO WARM UP....'])
 
 def excel():
         #os.system("start excel.exe")
@@ -21,13 +23,17 @@ def mainfunction(source):
         user = r.recognize_google(audio, language = "en-us", show_all=False)
         print(user)
         if user == "Excel":
+            call(['espeak', 'OPENING EXCEL..'])
             excel()
         elif user == "check":
+            call(['espeak', 'PERFORMING CHECK..'])
             internet()
         elif user == "media":
+            call(['espeak', 'OPENING MEDIA NOW...'])
             media()
     except:
-        print("can't understand you bro")
+        print("Cannot understand you. Please repeat your command.")
+        call(['espeak', 'CANNOT UNDERSTAND YOU. PLEASE REPEAT YOUR COMMAND...'])
 
 if __name__ == "__main__":
     r = sr.Recognizer()
